f26764a152e770a1f4599939f3c6732f7f232c4c
thivagarradhakrishnan <ajithivagar@gmail.com>
2025-01-27 23:19:35 +0530

    hrloo

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import LandingHeroSection from "@components/landing-page/LandingHeroSection";
 import LandingResearchSection from "@components/landing-page/LandingResearchSection";
-import LandingSolutionSection from "@components/landing-page/LandingSolutionSEction";
+import LandingSolutionSection from "@components/landing-page/LandingSolutionSection";
 import LandingLearnSection from "@components/landing-page/LandingLearnSection";
 import LandingCommunitySection from "@components/landing-page/LandingCommunitySection";
 export default function Home() {
